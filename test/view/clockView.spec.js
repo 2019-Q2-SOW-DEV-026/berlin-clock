b041bfdcf6bcddec073eccc575b9afb29cec7651
2019-Q2-SOW-DEV-026 <2019.q2.sow.dev.026@gmail.com>
2019-06-10 19:52:43 +0200

    test: Should display an error message when an error occurs

---
+++ test/view/clockView.spec.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import jsdom from 'mocha-jsdom';
+import ClockView from '../../src/view/clockView';
+
+describe('Berlin Clock View', () => {
+    jsdom({
+        url: 'http://localhost'
+    });
+
+    it('Should display an error message in case of an exception', () => {
+        const clockView = ClockView(document.body);
+        var div = document.createElement('div');
+        div.setAttribute('id', 'berlinClock');
+        document.body.appendChild(div);
+
+        clockView.setErrorMessage('Error Message');
+
+        expect(document.getElementById('berlinClock').innerHTML).eql('Error Message');
+    });
+});
