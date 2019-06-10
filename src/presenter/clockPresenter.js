2ba92cab106ac8990a8b2936dea28a012776c92d
2019-Q2-SOW-DEV-026 <2019.q2.sow.dev.026@gmail.com>
2019-06-10 17:48:39 +0200

    refactor: Group regex validation of seconds, minutes and hours into one

--- src/presenter/clockPresenter.js
+++ src/presenter/clockPresenter.js
@@ -5,13 +5,7 @@ const ClockPresenter = (_view, _model) => {
     const digitalTimeModel = _model;
 
     const isInValidTime = (time) => {
-        const seconds = time.split(':')[2];
-        const minutes = time.split(':')[1];
-        const hours = time.split(':')[0];
-
-        return  !/^([0-5][0-9])?$/.test(seconds) ||
-                !/^([0-5][0-9])?$/.test(minutes) ||
-                !/^([0-1]?[0-9]|2[0-4])?$/.test(hours);
+        return !/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(time);
     };
 
     const getBerlinTime = () => {
