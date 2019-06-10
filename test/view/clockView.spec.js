52574acb9f9ede6aba966f968b8b0146e47c2fbe
2019-Q2-SOW-DEV-026 <2019.q2.sow.dev.026@gmail.com>
2019-06-10 20:16:26 +0200

    test: Move initialization of clockView in beforeEach

--- test/view/clockView.spec.js
+++ test/view/clockView.spec.js
@@ -7,23 +7,23 @@ describe('Berlin Clock View', () => {
         url: 'http://localhost'
     });
 
-    it('Should display an error message in case of an exception', () => {
-        const clockView = ClockView(document.body);
+    let clockView;
 
+    beforeEach('', () => {
+        clockView = ClockView(document.body);
+    });
+
+    it('Should display an error message in case of an exception', () => {
         clockView.setErrorMessage('Error Message');
 
         expect(document.getElementById('berlinClock').innerHTML).eql('Error Message');
     });
 
     it('Should display seconds lamp', () => {
-        const clockView = ClockView(document.body);
-
         expect(document.getElementById('secondsLamp')).to.not.be.null;
     });
 
     it('Should turn on seconds lamp during even seconds', () => {
-        const clockView = ClockView(document.body);
-
         clockView.setBerlinTime('YOOOOOOOOOOOOOOOOOOOOOOO');
 
         let secondsLamp = document.querySelectorAll('#secondsLamp .light')[0];
