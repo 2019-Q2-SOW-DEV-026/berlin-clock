41dac08dbd113eb96263f1946fac82a08012c910
2019-Q2-SOW-DEV-026 <2019.q2.sow.dev.026@gmail.com>
2019-06-10 20:22:49 +0200

    feat: Should display five hours row with 4 lamps

--- src/templates/clock_template.js
+++ src/templates/clock_template.js
@@ -3,5 +3,11 @@ export default () => {
                 <div id="secondsLamp">
                     <div class="light"></div>
                 </div>
+                <div id="fiveHourRow">
+                    <div class="light"></div>
+                    <div class="light"></div>
+                    <div class="light"></div>
+                    <div class="light"></div>
+                </div>
             </div>`;
 };
--- test/view/clockView.spec.js
+++ test/view/clockView.spec.js
@@ -36,4 +36,8 @@ describe('Berlin Clock View', () => {
         let secondsLamp = document.querySelectorAll('#secondsLamp .light')[0];
         expect(secondsLamp.classList.contains('off')).to.be.true;
     });
+
+    it('Should display five hours row with 4 lamps', () => {
+        expect(document.querySelectorAll('#fiveHourRow .light').length).to.equal(4);
+    });
 });
