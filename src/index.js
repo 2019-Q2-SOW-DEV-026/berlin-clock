ce50bed42ee61fa3587563ded73a81d4446bbfb4
2019-Q2-SOW-DEV-026 <2019.q2.sow.dev.026@gmail.com>
2019-06-10 21:44:33 +0200

    feat: Call presenter to show the berlin clock every second

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-document.getElementById('container').innerHTML = 'Hello World';
+import ClockPresenter from './presenter/clockPresenter';
+import ClockView from './view/clockView';
+import DigitalTime from './model/digitalTime';
+
+const berlinClockContainer = document.getElementById('container');
+const clock = ClockPresenter(new ClockView(berlinClockContainer), new DigitalTime());
+
+let updateClock;
+
+(updateClock = () => {
+    clock.showTime();
+
+    window.requestAnimationFrame(updateClock);
+})();
